862dfed6fdca2da822b94e418992feb47db18652
Paul Spencer <paul.spencer@canvaspop.com>
2019-04-24 14:09:33 -0400

    Add build for alpine 3.7

---
+++ packaging/alpine_3.7/sources.mk
@@ -0,0 +1,11 @@
+ALPINE_SOURCES = \
+	packaging/alpine_3.7/APKBUILD \
+	packaging/alpine_3.7/jobber.initd \
+	packaging/alpine_3.7/jobber.post-deinstall \
+	packaging/alpine_3.7/jobber.post-install \
+	packaging/alpine_3.7/jobber.post-upgrade \
+	packaging/alpine_3.7/jobber.pre-deinstall \
+	packaging/alpine_3.7/Makefile \
+	packaging/alpine_3.7/pkgrel \
+	packaging/alpine_3.7/sources.mk \
+	packaging/alpine_3.7/Vagrantfile
